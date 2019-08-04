42f27bde03c3f6a8307c1d64327f4d210f4f8a35
Gregory Priznar <44954435+Gpriznar@users.noreply.github.com>
2019-08-04 18:33:36 -0500

    Weekend Updates - Date/Time Manipulation

--- hbd/src/components/Birthday.js
+++ hbd/src/components/Birthday.js
@@ -6,8 +6,11 @@ import YesHBD from './YesHBD';
 
 class Birthday extends Component {
   constructor() {
+    let today = new Date()
     super()
     this.state = {
+      month: "",
+      today: today,
       isItYourBirthday: 0,
     }
   }
@@ -28,6 +31,67 @@ class Birthday extends Component {
   }
 
 
+
+//Component
+
+  handleSetMonth = () => {
+      let today = this.state.today
+      let month = (today.getUTCMonth() + 1).toString()
+      console.log(this.state.month)
+      console.log(month)
+      if(month === 1) {
+        this.setState({
+          month: "Janurary"
+        })
+      } else if (month === 2){
+        this.setState({
+          month: "Feburary"
+        })
+      } else if (month === 3) {
+        this.setState({
+          month: "March"
+        })
+      } else if (month === 4) {
+        this.setState({
+          month: "April"
+        })
+      } else if (month === 5){
+        this.setState({
+          month: "May"
+        })
+      } else if (month === 6){
+        this.setState({
+          month: "June"
+        })
+      } else if (month === 7){
+        this.setState({
+          month: "July"
+        })
+      } else if (month === 8){
+        this.setState({
+          month: "August"
+        })
+      } else if (month === 9){
+        this.setState({
+          month: "September"
+        })
+      } else if (month === 10){
+        this.setState({
+          month: "October"
+        })
+      } else if (month === 11){
+        this.setState({
+          month: "November"
+        })
+      } else if (month === 12){
+        this.setState({
+          month: "December"
+        })
+      }
+
+}
+
+
 //Render
 
   render() {
@@ -37,12 +101,17 @@ class Birthday extends Component {
 
 
 if(this.state.isItYourBirthday === 0) {
+  let today = this.state.today
+  let day = (today.getUTCDate().toString())
+  let month = this.state.month
+  let year = (today.getFullYear().toString())
     body = (
       <div>
-      <h1> Is TODAY your Birthday? </h1>
+      <h1> Is {month} {day}, {year} your birthday? </h1>
       <div className="HBDButtons">
       <button onClick={this.handleYes}> Yes! </button>
       <button onClick={this.handleNo} > No! </button>
+      <button onClick={this.handleSetMonth} > Month </button>
       </div>
       </div>
     )
